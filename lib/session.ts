cf19e4c1f808e2a9303debae48f4eded160421a6
Ádám Klöczl <kloczl.adam@gmail.com>
2023-07-08 11:24:41 +0200

    finished login with google and jwt

--- lib/session.ts
+++ lib/session.ts
@@ -9,10 +9,16 @@ import { createUser, getUser } from './actions';
 
 export const authOptions: NextAuthOptions = {
   providers: [GoogleProvider({ clientId: process.env.GOOGLE_CLIENT_ID!, clientSecret: process.env.GOOGLE_CLIENT_SECRET! })],
-  /* jwt: {
-    encode: ({ secret, token }) => {},
-    decode: async ({ secret, token }) => {}
-  }, */
+  jwt: {
+    encode: ({ secret, token }) => {
+      const encodedToken = jsonwebtoken.sign({ ...token, iss: 'grafbase', exp: Math.floor(Date.now() / 1000) + 60 * 60 }, secret);
+      return encodedToken;
+    },
+    decode: async ({ secret, token }) => {
+      const decodedToken = jsonwebtoken.verify(token!, secret) as JWT;
+      return decodedToken;
+    }
+  },
   theme: {
     colorScheme: 'light',
     logo: '/logo.svg'
